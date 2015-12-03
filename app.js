d4f70f5ce7a0ccccb777159aa13d1020ac6968b1
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-12-03 14:39:18 -0700

    enter()

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-d3.json("food.json", function(error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+// d3.json("food.json", function(error, data) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+
+var dataset = [ 5, 10, 15, 20, 25 ];
+
+d3.select("body").selectAll("p")
+.data(dataset)
+.enter()
+.append("p")
+.text(function(d) {
+  return d;
 });
