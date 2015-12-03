70ff7e8d531fd9fc6053d2c2c8f1826bd6fca307
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-12-03 15:57:21 -0700

    Bar chart beginning.

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-// d3.json("food.json", function(error, data) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-
 var dataset = [ 5, 10, 15, 20, 25 ];
 
-d3.select("body").selectAll("p")
+d3.select("body").selectAll("div")
 .data(dataset)
 .enter()
-.append("p")
-.text(function(d) {
-  return "I can count up to " + d;
-})
-.style("color", function(d) {
-  if (d % 2 == 0) { // if datum is an even number
-    return "red";
-  } else { // if datum is an odd number
-    return "black";
-  }
+.append("div")
+.attr("class", "bar")
+.style("height", function(d) {
+  return d + "px";
 });
