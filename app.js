28d85eeab150b762bfa6d614cf229dd533baa9bd
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-12-06 15:01:07 -0700

    Earthquake bar chart

--- app.js
+++ app.js
@@ -1,68 +1,18 @@
-// d3.json("food.json", function(error, dataset) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(dataset);
-//     d3.select("body").selectAll("p")
-//     .data(dataset)
-//     .enter().append("p")
-//     .text(function(d) {
-//       return (d.Food);
-//     });
-//   }
-// });
-
-
-
-d3.json("https://bonus.ly/api/v1/users?access_token=296b9968b8c684d371232d6dfe64ca3d", function(error, dataset) {
+d3.json("http://soda.demo.socrata.com/resource/earthquakes.json?$where=magnitude > 5.5", function(error, dataset) {
   if (error) {
     console.log(error);
   } else {
     console.log(dataset);
-    var dataArray = [];
-    dataArray.push(dataset);
-    console.log(dataArray);
     d3.select("body").selectAll("p")
-    .data(dataArray)
-    .enter().append("p")
+    .data(dataset)
+    .enter().append("div")
+    .attr("class", "bar")
+    .style("width", function(d) {
+      var barWidth = (d.magnitude -5) * 500;
+      return barWidth + "px";
+    })
     .text(function(d) {
-      return (d.success);
-    });
+      return d.region;
+    })
   }
 });
-
-
-
-// d3.select("body").selectAll("p")
-// .data(dataset)
-// .enter().append("p")
-// .text(function(d) {return d;} );
-
-// var dataset = [ 5, 10, 15, 20, 25 ];
-
-// d3.select("body").selectAll("div")
-// .data(dataset)
-// .enter()
-// .append("div")
-// .attr("class", "bar")
-// .style("width", function(d) {
-//   var barWidth = d * 5; //Scale up by factor of 5
-//   return barWidth + "px";
-// })
-// .text(function(d) {
-//   return d;
-// })
-// .style("background-color", function(d) {
-//   return "rgb(0, 0, " + (d * 10) + ")";
-// });
-
-
-
-
-// d3.select("body")
-// .data(dataset)
-// .enter()
-// .append("p")
-// .text(function(d) {
-//   return d;
-// });
